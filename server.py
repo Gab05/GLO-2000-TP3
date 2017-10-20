28959f6012587f945066a64a37b3c96283945dac
GabCh <gabcha25@gmail.com>
2017-10-20 17:59:32 -0400

    Error.log

--- server.py
+++ server.py
@@ -1,15 +1,26 @@
 import socket, optparse, sys
 from socketUtil import recv_msg, send_msg
+from datetime import datetime
+
+
 #choisissez l’adresse avec l’option -a et le port avec -p
 parser = optparse.OptionParser()
 parser.add_option("-s", "--server", action="store_true", dest="serveur", default=False)
-parser.add_option("-d", "--destination", action="store", dest="dest", default="localhost")
-parser.add_option("-p", "--port", action="store", dest="port", type=int, default=1337)
+parser.add_option("-d", "--destination", action="store", dest="dest", default=False)
+parser.add_option("-p", "--port", action="store", dest="port", type=int, default=False)
 opts = parser.parse_args(sys.argv[1:])[0]
-if opts.serveur and opts.dest:
-    print("WRONG!")
+if opts.serveur and not opts.port:
+    print("Erreur : L’option -p est obligatoire")
+    file = open("Error.log", "a")
+    file.write(str(datetime.now()) + " -> " + "Erreur : L’option -p est obligatoire" + "\n")
+    file.close()
+elif opts.serveur and opts.dest:
+    print("Erreur : L’application ne peut pas utiliser -d et -s simultanément")
+    file = open("Error.log", "a")
+    file.write(str(datetime.now()) + " -> " + "Erreur : L’application ne peut pas utiliser -d et -s simultanément" + "\n")
+    file.close()
 else:
-    if opts.serveur: #mode serveur
+    if opts.serveur and opts.port: #mode serveur
         serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         serversocket.bind(("localhost", opts.port))
