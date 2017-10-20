f59a3dcee0e7f9bd1b15fdd839cc59831a4a9bce
GabCh <gabcha25@gmail.com>
2017-10-20 17:40:09 -0400

    Ajout des options et conditions de lancement

--- server.py
+++ server.py
@@ -1,28 +1,35 @@
 import socket, optparse, sys
-
-# choisissez le port avec l’option -p
+from socketUtil import recv_msg, send_msg
+#choisissez l’adresse avec l’option -a et le port avec -p
 parser = optparse.OptionParser()
-parser.add_option("-p", "--port", action="store", dest="port", type=int, default=1337)
 parser.add_option("-s", "--server", action="store_true", dest="serveur", default=False)
 parser.add_option("-d", "--destination", action="store", dest="dest", default="localhost")
-port = parser.parse_args(sys.argv[1:])[0].port
-# AF_INET: indique d’utiliser des adresses IPv4
-# SOCK_STREAM: indique d’utiliser le protocole TCP
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# permet de faire plusieurs communications de suite sur le meme port
-serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-# indique au socket d’ecouter sur le port selectionne
-serversocket.bind(("localhost", port))
-# demarre le socket
-serversocket.listen(5)
-print("Listening on port " + str(serversocket.getsockname()[1]))
-i = 0
-while True:
-    # un client se connecte au serveur
-    # s est un nouveau socket pour interagir avec le client
-
-    (s, address) = serversocket.accept()
-    # affichage du nombre de connection au serveur
-    i += 1
-    print(str(i) + "e connexion au serveur")
-    s.close()
+parser.add_option("-p", "--port", action="store", dest="port", type=int, default=1337)
+opts = parser.parse_args(sys.argv[1:])[0]
+if opts.serveur and opts.dest:
+    print("WRONG!")
+else:
+    if opts.serveur: #mode serveur
+        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serversocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        serversocket.bind(("localhost", opts.port))
+        serversocket.listen(5)
+        print("Listening on port " + str(serversocket.getsockname()[1]))
+        i = 0
+        while True:
+            (s, address) = serversocket.accept()
+            i += 1
+            print (str(i) + "e connexion au serveur")
+            send_msg(s, "Bonjour, mon nom est Serveur. Et vous?\n")
+            nom = recv_msg(s)
+            print("Le nom du client est " + nom)
+            send_msg(s, "Enchante, " + nom)
+            s.close()
+    else: #mode client
+        destination = (opts.address, opts.port)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(destination)
+        print(recv_msg(s))
+        send_msg(s, input())
+        print(recv_msg(s))
+        s.close()
